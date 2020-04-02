172e6b6838fd3d62ec35b6faec51c0ed4fa8a5c7
cchoudhari1@gmail.com <cchoudhari1@gmail.com>
2020-04-02 22:57:00 +0530

    [Chetan] Add. added implementation of testCase1 to pass zero to feet and should return equal

---
+++ QuantityMeasurement/QuantityMeasurement/QuantityMeasurementTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using QuantityMeasurementImplementation;
+
+namespace QuantityMeasurement
+{
+    public class QuantityMeasurementTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Givens the zero feet zero feet should return equal.
+        /// </summary>
+        [Test]
+        public void GivenZeroFeet_ZeroFeet_ShouldReturnEqual()
+        {
+            Feet feet = new Feet();
+            double act=feet.CheckForEqual(0);
+            Assert.AreEqual(0, act);
+        }
+    }
+}
---
+++ QuantityMeasurement/QuantityMeasurementImplementation/Feet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurementImplementation
+{
+    public class Feet
+    {
+        private double length;
+
+        public Feet()
+        {
+
+        }
+
+        public double CheckForEqual(double length)
+        {
+            return this.length = length;
+        }
+    }
+}
