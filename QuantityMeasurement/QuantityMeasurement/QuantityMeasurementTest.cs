32457f729522655f9a3e6ef1e76154710a1324be
cchoudhari1@gmail.com <cchoudhari1@gmail.com>
2020-04-03 23:30:23 +0530

    [Chetan] Add. added implementation of yard class to null check equality check and etc

--- QuantityMeasurement/QuantityMeasurement/QuantityMeasurementTest.cs
+++ QuantityMeasurement/QuantityMeasurement/QuantityMeasurementTest.cs
@@ -20,6 +20,10 @@ namespace QuantityMeasurement
             bool act=feet.CheckForEqual(feet);
             Assert.IsTrue(act);
         }
+
+        /// <summary>
+        /// Tests for equality null check should return true or false.
+        /// </summary>
         [Test]
         public void TestFor_EqualityNullCheck_ShouldReturnTrueOrFalse()
         {
@@ -27,6 +31,10 @@ namespace QuantityMeasurement
             bool act=feet.Equals(null);
             Assert.IsTrue(act);
         }
+
+        /// <summary>
+        /// Tests for reference check should return true or false.
+        /// </summary>
         [Test]
         public void TestFor_ReferenceCheck_ShouldReturnTrueOrFalse()
         {
@@ -35,6 +43,9 @@ namespace QuantityMeasurement
             Assert.IsTrue(act);
         }
 
+        /// <summary>
+        /// Tests for type check should return true or false.
+        /// </summary>
         [Test]
         public void TestFor_TypeCheck_ShouldReturnTrueOrFalse()
         {
@@ -43,6 +54,9 @@ namespace QuantityMeasurement
             Assert.IsTrue(act);
         }
 
+        /// <summary>
+        /// Tests for value equality should return value equal.
+        /// </summary>
         [Test]
         public void TestForValueEquality_ShouldReturnValueEqual()
         {
@@ -50,6 +64,10 @@ namespace QuantityMeasurement
             bool act =feet.CheckForEqual(feet);
             Assert.IsTrue(act);
         }
+
+        /// <summary>
+        /// Givens the zero to inch zero inch should retorn equality.
+        /// </summary>
         [Test]
         public void GivenZeroToInch_ZeroInchShouldRetorn_Equality()
         {
@@ -57,6 +75,10 @@ namespace QuantityMeasurement
             bool act=inch.CheckForEquality(inch);
             Assert.IsTrue(act);
         }
+
+        /// <summary>
+        /// Tests for inch equality null check should return true or false.
+        /// </summary>
         [Test]
         public void TestFor_InchEqualityNullCheck_ShouldReturnTrueOrFalse()
         {
@@ -64,6 +86,10 @@ namespace QuantityMeasurement
             bool act = inch.Equals(null);
             Assert.IsTrue(act);
         }
+
+        /// <summary>
+        /// Tests for inch reference check should return true or false.
+        /// </summary>
         [Test]
         public void TestFor_InchReferenceCheck_ShouldReturnTrueOrFalse()
         {
@@ -71,6 +97,10 @@ namespace QuantityMeasurement
             bool act = inch.Equals(inch);
             Assert.IsTrue(act);
         }
+
+        /// <summary>
+        /// Tests for inch type check should return true or false.
+        /// </summary>
         [Test]
         public void TestFor_InchTypeCheck_ShouldReturnTrueOrFalse()
         {
@@ -78,11 +108,51 @@ namespace QuantityMeasurement
             bool act = inch.Equals(inch.GetType());
             Assert.IsTrue(act);
         }
+
+        /// <summary>
+        /// Tests for value equality in inch should return value equal.
+        /// </summary>
         [Test]
         public void TestForValueEqualityInInch_ShouldReturnValueEqual()
         {
             Inch inch = new Inch(10);
             bool act=inch.CheckForEquality(inch);
+            Assert.IsTrue(act);
+        }
+
+        [Test]
+        public void GivenZeroToYard_ZeroYardShouldRetorn_Equality()
+        {
+            Yard yard = new Yard(0);
+            bool act = yard.CheckForEqual(yard);
+            Assert.IsTrue(act);
+        }
+        [Test]
+        public void GivenNullToYardEqualityNullCheck_ShouldReturnTrueOrFalse()
+        {
+            Yard yard = new Yard(0);
+            bool act = yard.Equals(null);
+            Assert.IsTrue(act);
+        }
+        [Test]
+        public void GivenReference_WhenCheck_ShouldReturnTrueOrFalse()
+        {
+            Yard yard = new Yard(0);
+            bool act = yard.Equals(yard);
+            Assert.IsTrue(act);
+        }
+        [Test]
+        public void GivenYardType_TypeCheck_ShouldReturnTrueOrFalse()
+        {
+            Yard yard = new Yard(0);
+            bool act = yard.Equals(yard.GetType());
+            Assert.IsTrue(act);
+        }
+        [Test]
+        public void GivenYardvalue_ToEqualityInYard_ShouldReturnValueEqual()
+        {
+            Yard yard = new Yard(10);
+            bool act = yard.CheckForEqual(yard);
             Assert.IsTrue(act);
         }
     }
---
+++ QuantityMeasurement/QuantityMeasurementImplementation/Yard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurementImplementation
+{
+    public class Yard
+    {
+        double yard;
+
+        public Yard(double yard)
+        {
+            this.yard = yard;
+        }
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+
+            if (obj == null || object.ReferenceEquals(obj, this) || obj.Equals(this.GetType()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public bool CheckForEqual(Yard yard)
+        {
+            if (this.yard.Equals(yard.yard))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
